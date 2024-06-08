68b119cfbf941d38165203408ac084bb4b613397
hasna000 <newbot859@gmail.com>
2024-06-08 14:23:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const toBool = (x) => x === 'true';
 const DATABASE_URL = process.env.DATABASE_URL === undefined ? "./database.db" : process.env.DATABASE_URL
 
 module.exports = {
-  AUDIO_DATA: process.env.AUDIO_DATA || 'ʟᴏᴋɪ-xᴇʀ;ᴊᴀʀᴠɪꜱ;https://i.imgur.com/ep3UbBP.jpg',
+  AUDIO_DATA: process.env.AUDIO_DATA || '𝐂𝚫𝚻 𝐒𝚸𝐘;ᴊᴀʀᴠɪꜱ;https://i.imgur.com/ep3UbBP.jpg',
   WARN_COUNT: process.env.WARN_COUNT || '3',
   LOGS: toBool(process.env.LOGS) || true,
   KOYEB_API: process.env.KOYEB_API || '',
@@ -27,16 +27,16 @@ module.exports = {
   RMBG_KEY: process.env.RMBG_KEY || false,
   BRANCH: 'main',
   PORT: toBool(process.env.PORT) || 8000,
-  STICKER_PACKNAME: process.env.STICKER_PACKNAME || 'ᴊᴀʀᴠɪꜱ;ᴡᴀʙᴏᴛ',
+  STICKER_PACKNAME: process.env.STICKER_PACKNAME || '𝐂𝚫𝚻 𝐒𝚸𝐘;𝐂𝚫𝚻 𝐒𝚸𝐘',
   AUTO_STATUS_VIEWS: toBool(process.env.AUTO_STATUS_VIEWS || 'false'),
   WELCOME_MSG: process.env.WELCOME_MSG || 'Hi @user Welcome to @gname',
   GOODBYE_MSG: process.env.GOODBYE_MSG || 'Hi @user It was Nice Seeing you',
-  AUTHOR: process.env.AUTHOR || 'ʟᴏᴋɪ-xᴇʀ',
-  MEDIA_DATA: process.env.MEDIA_DATA|| 'ʟɪꜱᴛ ᴍᴇɴᴜ;ᴊᴀʀᴠɪꜱ-ᴍᴅ;https://i.imgur.com/ltPPMTh.jpeg',
+  AUTHOR: process.env.AUTHOR || '𝛭𝑈𝛨𝛥𝛭𝛭𝛯𝐷',
+  MEDIA_DATA: process.env.MEDIA_DATA|| '𝛭𝑈𝛨𝛥𝛭𝛭𝛯𝐷;𝐂𝚫𝚻 𝐒𝚸𝐘;https://telegra.ph/file/f1ae2cd1d9d934d9c2465.jpg',
   SUDO: process.env.SUDO || '917025673121,',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || '',
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || '',
-  BOT_INFO: process.env.BOT_INFO || 'ᴊᴀʀᴠɪꜱ;ʟᴏᴋɪ-xᴇʀ;https://i.imgur.com/VIpl3q5.mp4',
-  WORK_TYPE: process.env.WORK_TYPE || 'private',
+  BOT_INFO: process.env.BOT_INFO || '𝐂𝚫𝚻 𝐒𝚸𝐘;𝛭𝑈𝛨𝛥𝛭𝛭𝛯𝐷;https://i.imgur.com/VIpl3q5.mp4',
+  WORK_TYPE: process.env.WORK_TYPE || 'public',
   DATABASE: DATABASE_URL === "./database.db" ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: "postgres", ssl: true, protocol: "postgres", dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
 };
